747d597532a9fcef674620ae3e1811ca42343ea3
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-02-25 10:35:44 -0500

    Create ameriskur.java

---
+++ ameriskur.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double ans = (n * 0.09144);
+        System.out.println(ans);
+
+    }
+}
