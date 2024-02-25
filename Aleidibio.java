f30179cede40d3b0e4ac660aca35977f60ea1283
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-02-25 10:33:55 -0500

    Create Aleidibio.java

---
+++ Aleidibio.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Aleidibio {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+
+        System.out.println(n3 - (n1 + n2));
+
+
+    }
+
+
+}
