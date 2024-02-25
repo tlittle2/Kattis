1f22a1044f35bf05d985b4a64548016a310aed9f
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-02-25 10:29:29 -0500

    Create reduplikation.java

---
+++ reduplikation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int num = scanner.nextInt();
+
+        for(int i = 0; i < num; i++){
+            System.out.print(s);
+        }
+
+
+    }
+
+
+}
