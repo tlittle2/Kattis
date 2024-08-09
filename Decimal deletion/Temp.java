459ce1b455b6b1edda30944ed4c479a5a3239cb5
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-08-09 14:34:56 -0400

    Create Temp.java

---
+++ Decimal deletion/Temp.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Temp {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        float n = sc.nextFloat();
+
+        System.out.println(Math.round(n));
+
+    }
+}
