930623c067c8ff6bbf0e79d3fcf769aedfb87dde
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-08-09 14:40:34 -0400

    Create Temp.java

---
+++ Bitte ein Bit/Temp.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Temp {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.nextLine();
+
+        System.out.println(n.charAt(0));
+        
+    }
+}
